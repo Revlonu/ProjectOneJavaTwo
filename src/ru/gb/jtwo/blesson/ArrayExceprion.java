dd65676343d047723f5c0e758aaee2c29e3a2735
Aleksandr MArtakov <buyliteru@gmail.com>
2020-05-19 13:19:38 +0300

    Задание по второму уроку

---
+++ src/ru/gb/jtwo/blesson/ArrayExceprion.java
@@ -0,0 +1,9 @@
+package ru.gb.jtwo.blesson;
+
+public class ArrayExceprion extends Exception {
+
+    public ArrayExceprion(String message){
+        super(String.valueOf(message));
+    }
+
+}
---
+++ src/ru/gb/jtwo/blesson/Data.java
@@ -0,0 +1,10 @@
+package ru.gb.jtwo.blesson;
+
+public class Data {
+    static String oneString = "10 3 1 2\n2 3 2 2\n5 6 7 1\n300 3 1 0";
+
+    public static String getOneString() {
+        return oneString;
+    }
+
+}
