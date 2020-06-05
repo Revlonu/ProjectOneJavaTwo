ddd301662fb5c95c82eb32187b1d72f24a2f79a6
Revlon <buyliteru@gmail.com>
2020-06-05 14:32:50 +0300

    Прописал обработку сообщения после авторизации

--- chat-client/src/ru/gb/chat/client/ClientGUI.java
+++ chat-client/src/ru/gb/chat/client/ClientGUI.java
@@ -11,14 +11,17 @@ import java.awt.event.ActionListener;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.Socket;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
-public class ClientGUI extends JFrame implements ActionListener, Thread.UncaughtExceptionHandler, SocketThreadListener {
+public class ClientGUI extends JFrame implements ActionListener, Thread.UncaughtExceptionHandler, SocketThreadListener { // создаём класс ClientGUI наследник JFrame окно с рамкой реализуем интерфейс ActionListener(слушатель)/Thread.UncaughtExceptionHandler(вывод ошибки над консолью)/SocketThreadListener(слушатель сокетов)
 
     private static final int WIDTH = 600;
     private static final int HEIGHT = 300;
 
     private final JTextArea log = new JTextArea();
-    private final JPanel panelTop = new JPanel(new GridLayout(2, 3));
+    private final JPanel panelTop = new JPanel(new GridLayout(2, 3)); //  создали JPanel где сказали что 2 строки и 3 колонны
     private final JTextField tfIPAddress = new JTextField("127.0.0.1");
     private final JTextField tfPort = new JTextField("8189");
     private final JCheckBox cbAlwaysOnTop = new JCheckBox("Always on top");
@@ -31,63 +34,64 @@ public class ClientGUI extends JFrame implements ActionListener, Thread.Uncaught
     private final JTextField tfMessage = new JTextField();
     private final JButton btnSend = new JButton("Send");
 
+    private final DateFormat DATE_FORMAT = new SimpleDateFormat("HH:mm:ss: ");
     private final JList<String> userList = new JList<>();
     private boolean shownIoErrors = false;
 
     SocketThread socketThread;
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
+        SwingUtilities.invokeLater(new Runnable() { // @REVLON вопрос почему не просто new Runnable
             @Override
-            public void run() {
-                new ClientGUI();
+            public void run() { //Запустили поток
+                new ClientGUI(); // создали в нём новый объект ClientGUI
             }
         });
     }
 
     private ClientGUI() {
-        Thread.setDefaultUncaughtExceptionHandler(this);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        setSize(WIDTH, HEIGHT);
-        log.setEditable(false);
-        JScrollPane scrollLog = new JScrollPane(log);
-        JScrollPane scrollUser = new JScrollPane(userList);
+        Thread.setDefaultUncaughtExceptionHandler(this); // выводим исключение в окно с клииентом, this, отмечаем какого объекта ошибки ловить
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // даём стандартную возможность закрывать окно
+        setLocationRelativeTo(null); // расположение окна если null то левый верхний край по центру
+        setSize(WIDTH, HEIGHT); // задали размеры окна
+        log.setEditable(false); //запретили редактировать log окно где сами сообщения в чате
+        JScrollPane scrollLog = new JScrollPane(log); // создали панель где разместили log окно где сами сообщения в чате
+        JScrollPane scrollUser = new JScrollPane(userList); // создали окно где раместили списки пользователей
         String[] users = {"user1", "user2", "user3", "user4", "user5",
-                "user_with_an_exceptionally_long_name_in_this_chat"};
-        userList.setListData(users);
-        scrollUser.setPreferredSize(new Dimension(100, 0));
-        cbAlwaysOnTop.addActionListener(this);
-        btnSend.addActionListener(this);
-        tfMessage.addActionListener(this);
-        btnLogin.addActionListener(this);
-        btnDisconnect.addActionListener(this);
+                "user_with_an_exceptionally_long_name_in_this_chat"}; // создали список для userList
+        userList.setListData(users); // закинули список пользователей в userList
+        scrollUser.setPreferredSize(new Dimension(100, 0)); // задали размер окна где список юзеров scrollUser
+        cbAlwaysOnTop.addActionListener(this); // добавили слушателя галке всегда ли сверху окно
+        btnSend.addActionListener(this); // добавили слушателя кнопке отправить сообщение
+        tfMessage.addActionListener(this); // добавили слушателя окну для ввода сообщения
+        btnLogin.addActionListener(this); // добавили слушателя кнопке логин / подключится
+        btnDisconnect.addActionListener(this); // добавили слушателя кнопке отключится
 
-        panelTop.add(tfIPAddress);
-        panelTop.add(tfPort);
-        panelTop.add(cbAlwaysOnTop);
-        panelTop.add(tfLogin);
-        panelTop.add(tfPassword);
-        panelTop.add(btnLogin);
+        panelTop.add(tfIPAddress);  // добавили в верхнюю панель окно для ввода ip
+        panelTop.add(tfPort); // добавили в верхнюю панель окно для ввода port
+        panelTop.add(cbAlwaysOnTop); // добавили в верхнюю панель галку сверху ли окно
+        panelTop.add(tfLogin); // добавили в верхнюю панель окно для ввода логина
+        panelTop.add(tfPassword);// добавили в верхнюю панель окно для ввода пароля
+        panelTop.add(btnLogin);// добавили в верхнюю панель кнопку логин
 
-        panelBottom.add(btnDisconnect, BorderLayout.WEST);
-        panelBottom.add(tfMessage, BorderLayout.CENTER);
-        panelBottom.add(btnSend, BorderLayout.EAST);
-        panelBottom.setVisible(false);
+        panelBottom.add(btnDisconnect, BorderLayout.WEST); // + кнопка дисконет внижнюю панель указывая расположение
+        panelBottom.add(tfMessage, BorderLayout.CENTER); // + окно для ввода сообщения по центру в нижнюю панель
+        panelBottom.add(btnSend, BorderLayout.EAST);// + кнопка отправить сообщение в нижнюю панель
+        panelBottom.setVisible(false); // сделали панель нижнюю не видимой
 
-        add(scrollLog, BorderLayout.CENTER);
+        add(scrollLog, BorderLayout.CENTER); // разместил панели указав их расположение
         add(scrollUser, BorderLayout.EAST);
         add(panelTop, BorderLayout.NORTH);
         add(panelBottom, BorderLayout.SOUTH);
 
-        setVisible(true);
+        setVisible(true); // отобразили внутри окна объекты
     }
 
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-        Object src = e.getSource();
-        if (src == cbAlwaysOnTop) {
+    public void actionPerformed(ActionEvent e) { // реализовали, унаследованный метод от ActionListener которые принимает события от слушателя
+        Object src = e.getSource(); // getSource запрашиваем объект с которым первоночально произошло событие и присваиваем src
+        if (src == cbAlwaysOnTop) { // слушаем что проиозошли и взависимости от этого действуем
             setAlwaysOnTop(cbAlwaysOnTop.isSelected());
         } else if (src == btnSend || src == tfMessage) {
             sendMessage();
@@ -98,6 +102,8 @@ public class ClientGUI extends JFrame implements ActionListener, Thread.Uncaught
         } else {
             throw new RuntimeException("Unknown source: " + src);
         }
+
+
     }
 
     private void connect() {
@@ -187,7 +193,14 @@ public class ClientGUI extends JFrame implements ActionListener, Thread.Uncaught
 
     @Override
     public void onReceiveString(SocketThread thread, Socket socket, String msg) {
-        putLog(msg);
+        String[] msgArr;
+        msgArr = msg.split("±");
+        if(msgArr[0].equals("/bcast")) { //Для сообщений, отправляемых всем
+            putLog(DATE_FORMAT.format(new Long(msgArr[1])) + " " + msgArr[3]);
+        } else {
+            putLog("Тип сообщения, не распознан!");
+        }
+        //putLog(msg);
     }
 
     @Override
